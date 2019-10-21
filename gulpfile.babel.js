9e836cff27d3574435c243bbb0d08fd218ab94cb
LunchTime-github <hiyoon9@gmail.com>
2019-10-21 12:45:11 +0900

    1021_update

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -2,12 +2,35 @@ import gulp from "gulp";
 import gpug from "gulp-pug";
 import del from "del";
 import ws from "gulp-webserver";
+import image from "gulp-image";
+import autop from "gulp-autoprefixer";
+import csso from "gulp-csso";
+import bro from "gulp-bro";
+import babelify from "babelify";
+import ghPages from "gulp-gh-pages-with-updated-gift";
+
+import sass from "gulp-sass";
+sass.compiler = require("node-sass");
 
 const routes = {
   pug: {
     watch: "src/**/*.pug",
     src: "src/*.pug",
-    dest: "build"
+    dest: "build/"
+  },
+  img: {
+    src: "src/img/*",
+    dest: "build/img"
+  },
+  scss: {
+    watch: "src/scss/**/*.scss",
+    src: "src/scss/style.scss",
+    dest: "build/css"
+  },
+  js: {
+    watch: "src/js/**/*.js",
+    src: "src/js/main.js",
+    dest: "build/js"
   }
 };
 
@@ -17,17 +40,61 @@ const pug = () =>
     .pipe(gpug())
     .pipe(gulp.dest(routes.pug.dest));
 
-const clean = () => del(["build"]);
+const clean = () => del(["build/", ".publish"]);
 
 const webserver = () =>
-  gulp.src("build").pipe(ws({ livereload: true, open: true }));
+  gulp.src("build/").pipe(ws({ livereload: true, open: true }));
+
+const img = () =>
+  gulp
+    .src(routes.img.src)
+    .pipe(image())
+    .pipe(gulp.dest(routes.img.dest));
+
+const style = () =>
+  gulp
+    .src(routes.scss.src)
+    .pipe(sass().on("error", sass.logError))
+    .pipe(
+      autop({
+        overrideBrowserslist: ["last 2 versions"]
+      })
+    )
+    .pipe(
+      csso({
+        restructure: false,
+        sourceMap: true,
+        debug: true
+      })
+    )
+    .pipe(gulp.dest(routes.scss.dest));
+
+const js = () =>
+  gulp
+    .src(routes.js.src)
+    .pipe(
+      bro({
+        transform: [
+          babelify.configure({ presets: ["@babel/preset-env"] }),
+          ["uglifyify", { global: true }]
+        ]
+      })
+    )
+    .pipe(gulp.dest(routes.js.dest));
+
+const gh = () => gulp.src("build/**/*").pipe(ghPages());
 
 const watch = () => {
   gulp.watch(routes.pug.watch, pug);
+  gulp.watch(routes.img.src, img);
+  gulp.watch(routes.scss.watch, style);
+  gulp.watch(routes.js.watch, js);
 };
 
-const prepare = gulp.series([clean]);
-const assets = gulp.series([pug]);
-const postDev = gulp.parallel([webserver, watch]);
+const prepare = gulp.series([clean, img]);
+const assets = gulp.series([pug, style, js]);
+const live = gulp.parallel([webserver, watch]);
 
-export const dev = gulp.series([prepare, assets, postDev]);
+export const build = gulp.series([prepare, assets]);
+export const dev = gulp.series([build, live]);
+export const deploy = gulp.series([build, gh, clean]);
